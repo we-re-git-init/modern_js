104d61b16e6505f1ca6f024e236528e713b359b1
Brian <riceb53@gmail.com>
2019-07-28 11:44:22 -0500

    add babel

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ console.log('i am in index.js');
 console.log(moment().format('dddd'));
 
 console.log(math.sqrt(-4));
+
+
+console.log('here is the new webpack');
+
+
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
